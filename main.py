948d714387859c338c49acb5a2f5b2e2ba5064a5
torreslucs23 <torreslucs23@gmail.com>
2023-11-05 15:07:28 -0300

    created variation 1

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from math import sqrt
 from random import shuffle
 
-def calculateDistance(x1,y1,x2,y2):
-    return sqrt((x1-x2)**2 + (y1-y2)**2)
 
+# calculate the euclidian distance
+def calculateDistance(p1,p2):
+    return sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
 
+#get the coordenates of txt
 def getCoordenates(entry):
     xString = entry[0].split(' ')
     yString = entry[1].split(' ')
@@ -17,7 +19,7 @@ def getCoordenates(entry):
 
 
     
-
+#create a matrix of all distances
 def generateDistances(positions):
     distances = [[0,0,0] for i in range(len(positions))]
 
@@ -30,13 +32,13 @@ def generateDistances(positions):
 def initialState2(positions):
     return shuffle(positions)
 
-def operation1(positions, id1, id2):
+def operator1(positions, id1, id2):
     x = positions[id1]
     positions[id1] = positions[id2]
     positions[id2] = x
     return positions
 
-def operation2(positions, id1, id2):
+def operator2(positions, id1, id2):
     new_positions = []
     for i in range(id1):
         new_positions.append(positions[i])
@@ -48,11 +50,56 @@ def operation2(positions, id1, id2):
     return new_positions
 
 
-print(generateDistances([(0,0), (0,1), (1,0)]))
+def calculateCost(sequence):
+    cost = 0
+    for i in range(len(sequence)):
+        if i == len(sequence) -1:
+            cost += calculateDistance(sequence[i], sequence[0])
+            break
+        cost += calculateDistance(sequence[i], sequence[i+1])
+    return cost
 
 
-archive = open('entradas.txt', 'r')
 
-s = archive.readlines()
 
-print(s)
+archive = open('entries.txt', 'r')
+
+coordenates = getCoordenates(archive.readlines())
+
+
+
+
+
+#variation1: initial state 1 with operator 1 and no random neighbor
+def variation1(coordenates):
+    aux = []
+    cost = 0
+    while True:
+        flag = False
+        cost = calculateCost(coordenates)
+        print(cost)
+        for i in range(len(coordenates)):
+            for w in range(i, len(coordenates)):
+                aux = operator1(coordenates, i, w)
+                if calculateCost(aux) < cost:
+                    coordenates = aux.copy()
+                    flag = True
+                    cost = calculateCost(coordenates)
+                    break
+            if flag == True:
+                break
+        if flag == False:
+            break
+    return (cost, coordenates)
+
+
+print(variation1(coordenates))
+
+
+
+            
+
+
+#coordenates 
+
+
