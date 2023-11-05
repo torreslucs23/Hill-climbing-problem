f125bb0b09ff99c0d9038e4228b77c4308bf85aa
torreslucs23 <torreslucs23@gmail.com>
2023-11-05 19:28:01 -0300

    added hill_climbing_funcitons, main loop and generator of the table

---
+++ hill_climbing_functions.py
@@ -0,0 +1,207 @@
+from math import sqrt
+from random import shuffle
+from random import randint
+
+
+# calculate the euclidian distance
+def calculateDistance(p1,p2):
+    return sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+
+#get the coordenates of txt
+def getCoordenates(entry):
+    xString = entry[0].split(' ')
+    yString = entry[1].split(' ')
+    coordenates = []
+    for i in range(0, len(xString)):
+        x = float(xString[i])
+        y = float(yString[i])
+        coordenates.append((x,y))
+    return coordenates
+
+#aplies the operator 1
+def operator1(positions, id1, id2):
+    x = positions[id1]
+    positions[id1] = positions[id2]
+    positions[id2] = x
+    return positions
+
+#aplies the operator 2
+def operator2(positions, id1, id2):
+    new_positions = []
+    for i in range(id1):
+        new_positions.append(positions[i])
+    for i in range(id2, id1-1, -1):
+        new_positions.append(positions[i])
+    for i in range(id2+1, len(positions)):
+        new_positions.append(positions[i])
+    
+    return new_positions
+
+
+#calculate the cost of a sequence
+def calculateCost(sequence):
+    cost = 0
+    for i in range(len(sequence)):
+        if i == len(sequence) -1:
+            cost += calculateDistance(sequence[i], sequence[0])
+            break
+        cost += calculateDistance(sequence[i], sequence[i+1])
+    return cost
+
+
+
+#variation1: initial state 1 with operator 1 and no random neighbor
+def variation1(coordenates):
+    coord = coordenates.copy()
+    aux = []
+    cost = 0
+    while True:
+        flag = False
+        cost = calculateCost(coord)
+        print(cost)
+        for i in range(len(coord)):
+            for w in range(i, len(coord)):
+                aux = operator1(coord, i, w)
+                if calculateCost(aux) < cost:
+                    coord = aux.copy()
+                    flag = True
+                    cost = calculateCost(coord)
+                    break
+            if flag == True:
+                break
+        if flag == False:
+            break
+    cost = calculateCost(coord)
+    print(cost, coord)
+    print("")
+    return cost
+
+
+#variation 2: initial state 1 with operator 1 and random neighbor
+def variation2(coordenates):
+    coord = coordenates.copy()
+    aux = []
+    cost = 0
+
+    #n_cities just calculate the lenght of coordenates(cities)
+    n_cities = len(coord)
+
+    #this is a list of visited indexes for cut repetition on the random states
+    visited_indexes = []
+    while True:
+        cost = calculateCost(coord)
+        flag = False
+        print(cost)
+        while len(visited_indexes) < (n_cities**2 - n_cities)/2:
+            x = randint(0, n_cities-1)
+            y = randint(0, n_cities-1)
+            if sorted((x,y)) in visited_indexes:
+                continue
+
+            visited_indexes.append(sorted((x,y)))
+            aux = operator1(coord, x, y)
+            if calculateCost(aux) < cost:
+                coord = aux.copy()
+                flag = True
+                break
+        if flag == False:
+            break
+    calculateCost(coord)
+    print(coord,cost)
+    print(" ")
+    return cost
+
+
+
+
+#variation 3: initial state 1 with operator 2 and no random neighbor
+#notice that this variation is similar to variation 1, but just replacing the operator 2 above operator 1
+def variation3(coordenates):
+    coord = coordenates.copy()
+    aux = []
+    cost = 0
+    while True:
+        flag = False
+        cost = calculateCost(coord)
+        print(cost)
+        for i in range(len(coord)):
+            for w in range(i, len(coord)):
+                aux = operator2(coord, i, w)
+                if calculateCost(aux) < cost:
+                    coord = aux.copy()
+                    flag = True
+                    cost = calculateCost(coord)
+                    break
+            if flag == True:
+                break
+        if flag == False:
+            break
+    cost = calculateCost(coord)
+    print(cost, coord)
+    print("")
+    return cost
+
+
+#variation 4: initial state 1 with operator 2 and random neighbor
+#notice that this variation is similar to variation 2, but just replacing the operator 2 above operator 1
+def variation4(coordenates):
+    coord = coordenates.copy()
+    aux = []
+    cost = 0
+
+    #n_cities just calculate the lenght of coordenates(cities)
+    n_cities = len(coord)
+
+    #this is a list of visited indexes for cut repetition on the random states
+    visited_indexes = []
+    while True:
+        cost = calculateCost(coord)
+        flag = False
+        print(cost)
+        while len(visited_indexes) < (n_cities**2 - n_cities)/2:
+            x = randint(0, n_cities-1)
+            y = randint(0, n_cities-1)
+            if sorted((x,y)) in visited_indexes:
+                continue
+
+            visited_indexes.append(sorted((x,y)))
+            aux = operator2(coord, x, y)
+            if calculateCost(aux) < cost:
+                coord = aux.copy()
+                flag = True
+                break
+        if flag == False:
+            break
+    cost = calculateCost(coord)
+    print(coordenates, cost)
+    print(" ")
+    return cost
+
+#variation 5: initial state 2 with operator 1 and no random neighbor
+def variation5(coordenates):
+    shuffle(coordenates)
+    return variation1(coordenates)
+
+#variation 6: initial state 2 with operator 1 and random neighbor
+def variation6(coordenates):
+    shuffle(coordenates)
+    return variation2(coordenates)
+
+
+#variation 7: initial state 2 with operator 2 and no random neighbor
+def variation7(coordenates):
+    shuffle(coordenates)
+    return variation3(coordenates)
+
+
+#variation 8: initial state 2 with operator 2 and random neighbor
+def variation8(coordenates):
+    shuffle(coordenates)
+    return variation4(coordenates)
+
+
+archive = open('entries.txt', 'r')
+
+coordenates = getCoordenates(archive.readlines())
+
+print(variation4(coordenates))
--- main.py
+++ main.py
@@ -1,223 +1,42 @@
-from math import sqrt
-from random import shuffle
-from random import randint
+from hill_climbing_functions import *
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-# calculate the euclidian distance
-def calculateDistance(p1,p2):
-    return sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
-
-#get the coordenates of txt
-def getCoordenates(entry):
-    xString = entry[0].split(' ')
-    yString = entry[1].split(' ')
-    coordenates = []
-    for i in range(0, len(xString)):
-        x = float(xString[i])
-        y = float(yString[i])
-        coordenates.append((x,y))
-    return coordenates
-
-
-    
-#create a matrix of all distances
-def generateDistances(positions):
-    distances = [[0,0,0] for i in range(len(positions))]
-
-    for i in range(len(positions)):
-        print(i)
-        for w in range(i,len(positions)):
-            distances[i][w] = calculateDistance(positions[i][0], positions[i][1], positions[w][0], positions[w][1])
-    return distances
-
-def initialState2(positions):
-    return shuffle(positions)
-
-def operator1(positions, id1, id2):
-    x = positions[id1]
-    positions[id1] = positions[id2]
-    positions[id2] = x
-    return positions
-
-def operator2(positions, id1, id2):
-    new_positions = []
-    for i in range(id1):
-        new_positions.append(positions[i])
-    for i in range(id2, id1-1, -1):
-        new_positions.append(positions[i])
-    for i in range(id2+1, len(positions)):
-        new_positions.append(positions[i])
-    
-    return new_positions
-
-
-def calculateCost(sequence):
-    cost = 0
-    for i in range(len(sequence)):
-        if i == len(sequence) -1:
-            cost += calculateDistance(sequence[i], sequence[0])
-            break
-        cost += calculateDistance(sequence[i], sequence[i+1])
-    return cost
-
-
-
-
+#read the entries.txt
 archive = open('entries.txt', 'r')
 
 coordenates = getCoordenates(archive.readlines())
 
+#list of variations functions
+variations = [variation1, variation2, variation3,
+            variation4, variation5, variation6,
+            variation7, variation8]
+
+#results of variations
+variations_results = [[] for i in range(8)]
 
 
+#the main loop
+for i in range(8):
+    for w in range(30):
+        variations_results[i].append(variations[i](coordenates))
+    variations_results[i].sort()
 
 
-#variation1: initial state 1 with operator 1 and no random neighbor
-def variation1(coordenates):
-    coord = coordenates.copy()
-    aux = []
-    cost = 0
-    while True:
-        flag = False
-        cost = calculateCost(coord)
-        print(cost)
-        for i in range(len(coord)):
-            for w in range(i, len(coord)):
-                aux = operator1(coord, i, w)
-                if calculateCost(aux) < cost:
-                    coord = aux.copy()
-                    flag = True
-                    cost = calculateCost(coord)
-                    break
-            if flag == True:
-                break
-        if flag == False:
-            break
-    print(cost, coord)
-    print("")
+#this block of code create a pandas dataframe to show the results on the terminal
+df = pd.DataFrame(variations_results)
 
+df = df.transpose()
 
-#variation 2: initial state 1 with operator 1 and random neighbor
-def variation2(coordenates):
-    coord = coordenates.copy()
-    aux = []
-    cost = 0
+df.columns = ["variation " + str(i) for i in range(1,9)]
 
-    #n_cities just calculate the lenght of coordenates(cities)
-    n_cities = len(coord)
+print(df)
 
-    #this is a list of visited indexes for cut repetition on the random states
-    visited_indexes = []
-    while True:
-        cost = calculateCost(coord)
-        flag = False
-        print(cost)
-        while len(visited_indexes) < (n_cities**2 - n_cities)/2:
-            x = randint(0, n_cities-1)
-            y = randint(0, n_cities-1)
-            if sorted((x,y)) in visited_indexes:
-                continue
+#this block of code writes the table in a file called table.txt
+variations_results = list(map(list, zip(*variations_results)))
 
-            visited_indexes.append(sorted((x,y)))
-            aux = operator1(coord, x, y)
-            if calculateCost(aux) < cost:
-                coord = aux.copy()
-                flag = True
-                break
-        if flag == False:
-            break
-    print(coord,cost)
-    print(" ")
-
-
-
-#variation 3: initial state 1 with operator 2 and no random neighbor
-#notice that this variation is similar to variation 1, but just replacing the operator 2 above operator 1
-def variation3(coordenates):
-    coord = coordenates.copy()
-    aux = []
-    cost = 0
-    while True:
-        flag = False
-        cost = calculateCost(coord)
-        print(cost)
-        for i in range(len(coord)):
-            for w in range(i, len(coord)):
-                aux = operator2(coord, i, w)
-                if calculateCost(aux) < cost:
-                    coord = aux.copy()
-                    flag = True
-                    cost = calculateCost(coord)
-                    break
-            if flag == True:
-                break
-        if flag == False:
-            break
-    print(cost, coord)
-    print("")
-
-
-#variation 4: initial state 1 with operator 2 and random neighbor
-#notice that this variation is similar to variation 2, but just replacing the operator 2 above operator 1
-def variation4(coordenates):
-    coord = coordenates.copy()
-    aux = []
-    cost = 0
-
-    #n_cities just calculate the lenght of coordenates(cities)
-    n_cities = len(coord)
-
-    #this is a list of visited indexes for cut repetition on the random states
-    visited_indexes = []
-    while True:
-        cost = calculateCost(coord)
-        flag = False
-        print(cost)
-        while len(visited_indexes) < (n_cities**2 - n_cities)/2:
-            x = randint(0, n_cities-1)
-            y = randint(0, n_cities-1)
-            if sorted((x,y)) in visited_indexes:
-                continue
-
-            visited_indexes.append(sorted((x,y)))
-            aux = operator2(coord, x, y)
-            if calculateCost(aux) < cost:
-                coord = aux.copy()
-                flag = True
-                break
-        if flag == False:
-            break
-    print(coord,cost)
-    print(" ")
-
-#variation 5: initial state 2 with operator 1 and no random neighbor
-def variation5(coordenates):
-    shuffle(coordenates)
-    variation1(coordenates)
-
-#variation 6: initial state 2 with operator 1 and random neighbor
-def variation6(coordenates):
-    shuffle(coordenates)
-    variation2(coordenates)
-
-
-#variation 7: initial state 2 with operator 2 and no random neighbor
-def variation7(coordenates):
-    shuffle(coordenates)
-    variation3(coordenates)
-
-
-#variation 8: initial state 2 with operator 2 and random neighbor
-def variation8(coordenates):
-    shuffle(coordenates)
-    variation4(coordenates)
-
-print(variation8(coordenates))
-
-
-
-            
-
-
-#coordenates 
-
-
+with open('table.txt', 'w') as file:
+    file.write("Tabela de Dados:\n")
+    for row in variations_results:
+        file.write('\t'.join([str(elem) for elem in row]) + '\n')
